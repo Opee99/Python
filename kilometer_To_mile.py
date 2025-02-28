af9301246052c6b8a0f2baaac29f0d249d735d4a
Mubtasim Fuad Opee <96126850+Opee99@users.noreply.github.com>
2025-02-28 19:56:20 +0600

    Delete kilometer_To_mile.py

--- kilometer_To_mile.py
+++
@@ -1,9 +0,0 @@
-# Taking value of kilometer from the user
-km = float(input("Enter the value in kilometers: "))
-
-# conversion factor
-conv_factor = 0.621371
-
-# calculate miles
-miles = km * conv_factor
-print('%0.2f kilometers is equal to %0.2f miles' %(km,miles))
